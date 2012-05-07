15b49801d276a2546c68d0380157939a22356194
TonyT429 <TonyT429@gmail.com>
2012-05-07 03:45:20 -0700

    Had issues with my rename of my js file in my html file.  Having issues with boolean function - need to come back to it tomorrow - I am doing it wrong

--- Torrez_Anthony_assignment2.js
+++ Torrez_Anthony_assignment2.js
@@ -1,1 +1,24 @@
+// SDI 1205 Assignment Week 2
+// Student: Anthony Torrez
+// Date: May 10th , 2012
+
+// Set up Variables
+
+var bool_Var = false
+
+;
+
+function my_Bool() {
+	if (bool_Var = true) {
+    	var bool_Ret = 4.50;
+    } else {
+    	var bool_Ret = 4.00;
+    }
+    console.log(bool_Ret)
+};
+
+my_Bool();
+
+
+
 alert("JavaScript works!");
