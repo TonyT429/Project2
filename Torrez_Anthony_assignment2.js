af7840d4c29785ad0dc698d0128c1953a256cfa0
TonyT429 <TonyT429@gmail.com>
2012-05-09 03:13:10 -0700

    gotta think of good arguments for my Boolean that make sense

--- Torrez_Anthony_assignment2.js
+++ Torrez_Anthony_assignment2.js
@@ -2,23 +2,46 @@
 // Student: Anthony Torrez
 // Date: May 10th , 2012
 
-// Set up Variables
 
-var bool_Var = false
+console.log("I crack my eyes open and the clock say 9 am.  And so the day begins.  First things first, get up and check to see if there's ",
+					"enough coffee beans to make a pot.  If not, I'll have to settle for tea.  Let's see, I need 4 scoops to make a pot.");
+// Procedure
 
-;
-
-function my_Bool() {
-	if (bool_Var = true) {
-    	var bool_Ret = 4.50;
-    } else {
-    	var bool_Ret = 4.00;
-    }
-    console.log(bool_Ret)
+var makeCoffee = function (beans) {
+	
+	if (beans <= 4) {
+		var makePot = ("Yay! There is enough beans to make a pot of coffee");
+		} else {
+		var makePot = ("Hmmm...  Not enough beans for a pot of coffee, guess it's tea today.");
+		}
+		return(makePot);
 };
+makeCoffee(3);
 
-my_Bool();
+console.log("Well, while I wait for the coffee to brew, I should jump in the shower.  Should I shave first?"),
+
+// Boolean
+
+var myScruff = function (stubble, reflection)
+
+	if (stubble = "
+
+// Number
+
+// String
+
+// Array
+
+var stubbleFace = function (newEmail,  
 
 
+
+var 
+
+/*
+
+
+// recheck the boolean function on the chart - need two variables to compare!
 
 alert("JavaScript works!");
+*/
