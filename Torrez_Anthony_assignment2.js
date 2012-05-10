9269194bf2309b1c32e9df08b1ecf44e0cebcb37
TonyT429 <TonyT429@gmail.com>
2012-05-09 18:26:23 -0700

    consulting Josh

--- Torrez_Anthony_assignment2.js
+++ Torrez_Anthony_assignment2.js
@@ -5,40 +5,42 @@
 
 console.log("I crack my eyes open and the clock say 9 am.  And so the day begins.  First things first, get up and check to see if there's ",
 					"enough coffee beans to make a pot.  If not, I'll have to settle for tea.  Let's see, I need 4 scoops to make a pot.");
+					
 // Procedure
 
 var makeCoffee = function (beans) {
-	
-	if (beans <= 4) {
-		var makePot = ("Yay! There is enough beans to make a pot of coffee");
+	if (beans >= 4) {
+		console.log("Yay! There is enough beans to make a pot of coffee");
 		} else {
-		var makePot = ("Hmmm...  Not enough beans for a pot of coffee, guess it's tea today.");
+		console.log("Hmmm...  Not enough beans for a pot of coffee, guess it's tea today.");
 		}
-		return(makePot);
 };
+
 makeCoffee(3);
 
-console.log("Well, while I wait for the coffee to brew, I should jump in the shower.  Should I shave first?"),
+makeCoffee();
+console.log(" ");
+console.log("Well, while I wait for the coffee to brew, perhaps I should check my blood sugars. ");
 
 // Boolean
 
-var myScruff = function (stubble, reflection)
+var checkBlood = function (testStrip, lancet) {
+	  if (testStrip >=1 && lancet >= 1) {
+		   var testBlood = ("I have " + testStrip + " test strips and " + lancet + " lancets, so I can test my blood");
+		   } else {
+		   var testBlood = ("I have " + testStrip + " test strips and " + lancet + " lancets, so I am unable to test my blood");
+		   }
+		   return testBlood;
+};
+checkBlood(0 , 10);
 
-	if (stubble = "
-
+/*
 // Number
 
 // String
 
 // Array
 
-var stubbleFace = function (newEmail,  
-
-
-
-var 
-
-/*
 
 
 // recheck the boolean function on the chart - need two variables to compare!
