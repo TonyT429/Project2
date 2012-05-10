4a1cb542ca6e86620dcb9e9528628f89dc15d5dc
TonyT429 <TonyT429@gmail.com>
2012-05-10 03:32:50 -0700

    still having issues with my script. Individually, my functions work fine and give the correct console output, but when I put them together I lose the returned values'

--- Torrez_Anthony_assignment2.js
+++ Torrez_Anthony_assignment2.js
@@ -2,41 +2,75 @@
 // Student: Anthony Torrez
 // Date: May 10th , 2012
 
-
-console.log("I crack my eyes open and the clock say 9 am.  And so the day begins.  First things first, get up and check to see if there's ",
-					"enough coffee beans to make a pot.  If not, I'll have to settle for tea.  Let's see, I need 4 scoops to make a pot.");
+var cash = 50
+	boxLancets = 10.99,
+	boxTestStrips = 25.99,
+	treats1 = "MilkBones",
+	treats2 = "15lb. Bag of Beneful",
+	treats3 = "Chicken Jerky";
+	
+	
+	  
+	  
+console.log("I crack my eyes open and the clock say 9 am.  And so the day begins.  First things first, get up and check to see if there's enough coffee beans to make a pot.  If not, I'll have to settle for tea.  Let's see, I need 4 scoops to make a pot.");
 					
 // Procedure
 
 var makeCoffee = function (beans) {
 	if (beans >= 4) {
-		console.log("Yay! There is enough beans to make a pot of coffee");
+		console.log("Yay! I have " + beans + " scoops.  There is enough beans to make a pot of coffee");
 		} else {
-		console.log("Hmmm...  Not enough beans for a pot of coffee, guess it's tea today.");
+			console.log("Hmmm...  Only " + beans + " scoops.  Not enough beans for a pot of coffee, guess it's tea today.");
 		}
 };
 
-makeCoffee(3);
+makeCoffee(5);
 
-makeCoffee();
-console.log(" ");
-console.log("Well, while I wait for the coffee to brew, perhaps I should check my blood sugars. ");
+console.log('Well, while I wait for the coffee to brew, perhaps I should check my blood sugars.  I am running low on test supplies, I hope I have enough for today\'s test');
 
 // Boolean
 
 var checkBlood = function (testStrip, lancet) {
-	  if (testStrip >=1 && lancet >= 1) {
+	  if (testStrip >= 1 && lancet >= 1) {
 		   var testBlood = ("I have " + testStrip + " test strips and " + lancet + " lancets, so I can test my blood");
 		   } else {
-		   var testBlood = ("I have " + testStrip + " test strips and " + lancet + " lancets, so I am unable to test my blood");
+		   var testBlood = ("I have " + testStrip + " test strips and " + lancet + " lancets, so I am unable to test my blood at this time. But my blood sugar averages have been good, so I'll just pay strict attention to what I eat until I can get more test supplies");
 		   }
 		   return testBlood;
 };
-checkBlood(0 , 10);
+checkBlood(4 , 0);
 
-/*
+console.log('As I grab a cup of coffee, I need to go buy some more test supplies.  Let me see if I have enough cash or else I\'ll have to put it on my card.'); 
+
 // Number
 
+function myWallet () {
+	  var change = (cash - boxTestStrips - boxLancets);
+	  var myChange = change.toFixed(2);
+    return ("$" + myChange);
+	  
+};
+myWallet();
+
+console.log("I think I have enough money left to buy the dogs something");
+
+// String
+
+function cruisePetAisle () {
+	var buyTreats = ("treats1" + "treats2" + "treats3");
+	return buyTreats
+};
+cruisePetAisle()
+
+alert("JavaScript works!");
+
+
+
+
+
+
+
+/*
 // String
 
 // Array
@@ -45,5 +79,5 @@ checkBlood(0 , 10);
 
 // recheck the boolean function on the chart - need two variables to compare!
 
-alert("JavaScript works!");
+
 */
